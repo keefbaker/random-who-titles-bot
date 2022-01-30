05a80fcd2b75f23a03a1d73689ee7884d8ed427c
Keith Baker <keef.baker@n5md.com>
2022-01-30 17:51:45 +0000

    Update monster.py

--- rw/monster.py
+++ rw/monster.py
@@ -1,16 +1,16 @@
 import random
 
 monsters = [
-    {"s": "dalek", "p": "daleks"},
-    {"s": "cybernam", "p": "cybermen"},
-    {"s": "sea devil", "p": "sea devils"},
+    {"s": "Dalek", "p": "Daleks"},
+    {"s": "Cyberman", "p": "Cybermen"},
+    {"s": "Sea Devil", "p": "Sea Devils"},
     {"s": "angel", "p": "angels"},
     {"s": "silurian", "p": "silurians"},
     {"s": "master", "p": "master"},
     {"s": "sontaran", "p": "sontarans"},
     {"s": "rutan", "p": "rutans"},
     {"s": "bandrill", "p": "bandrills"},
-    {"s": "vashta nerada", "p": "vashta nerada"},
+    {"s": "Vashta Nerada", "p": "Vashta Nerada"},
     {"s": "dinosaur", "p": "dinosaurs"},
     {"s": "planet", "p": "planets"},
     {"s": "doctor", "p": "doctors"},
@@ -21,7 +21,7 @@ monsters = [
 ]
 
 monsters += [{"s": "dalek", "p": "daleks"}] * 5
-monsters += [{"s": "cybernam", "p": "cybermen"}] * 4
+monsters += [{"s": "cyberman", "p": "cybermen"}] * 4
 monsters += [{"s": "master", "p": "master"}] * 3
 monsters += [{"s": "sontaran", "p": "sontarans"}] * 2
 
